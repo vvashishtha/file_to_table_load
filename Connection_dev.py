f4f9cda7aafeed148d56a3f335b73076e7d137ec
VASHISHTHA <vavashishtha@paypal.com>
2019-02-11 17:56:49 +0530

    Signed-off-by: VASHISHTHA <vavashishtha@paypal.com>

---
+++ Connection_dev.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 15:59:23 2019
+
+@author: vavashishtha
+this is foaasdr DATAtrek
+"""
+
+
+import sys
+import cx_Oracle
+import pandas as pd
+
+file_name='source_data_test.xlsx'#sys.argv[1]
+sheets_name='Sheet1'#sys.argv[2]
+Mapping_name='Mapping_test.xlsx'
+
+df_excel = pd.read_excel(file_name, sheet_name=sheets_name)
+df_excel1= pd.read_excel(file_name, sheet_name=sheets_name)
+df_Mapping=pd.read_excel(Mapping_name, sheet_name=sheets_name)
+df_excel['NaN']=float('NAN')
+dic_excel=df_excel.to_dict('series')
+lol = df_excel.values.tolist()
+Excel_column_list=list(df_excel)
+Mapping_column_list=df_Mapping['Excel_Column_Name'].tolist()
+Table_Column_Name=df_Mapping['Table_Column_Name'].tolist()
+
+def printf (format,*args):
+    sys.stdout.write (format % args)
+
+def printException (exception):
+  error, = exception.args
+  printf ("Error code = %s\n",error.code);
+  printf ("Error message = %s\n",error.message);
+
+def CompareTwoList(List1,List2):
+  list_index=[]
+  flag_available=1
+  for item1 in List2:
+    flag_available=1
+    for item2 in List1:
+        if (item1==item2):
+            list_index.append(List1.index(item2))
+            flag_available=0
+    if (flag_available==1):
+        list_index.append(-1)
+
+  return list_index
+
+
+ls_index=CompareTwoList(Excel_column_list,Mapping_column_list)
+
+
+ls_cols = df_excel.columns.tolist()
+ls_col_list=[]
+
+for j in ls_index:
+    if(j==-1):
+        ls_col_list.append('NaN')
+    else:
+        ls_col_list.append(ls_cols[j])
+
+df_excel = df_excel[ls_col_list]
+print(df_excel.head())
+
+Sql_execute="insert into Demo_table ("
+for j in Table_Column_Name:
+    Sql_execute=Sql_execute+str(j)+', '
+Sql_execute = Sql_execute[:-2]
+Sql_execute=Sql_execute+' ) values ('
+for i in ls_index:
+    Sql_execute=Sql_execute+':'+str(i)+', '
+Sql_execute = Sql_execute[:-2]
+Sql_execute=Sql_execute+' )'
+
+#username = 'scott'
+#password = 'tiger'
+databaseName = "DATATREK"
+print('vaibhav')
+conn_str = 'etlapp/Et1$ppJDEV654456@janatadev'
+try:
+    conn = cx_Oracle.connect(conn_str)
+except cx_Oracle.DatabaseError as exception:
+  printf ('Failed to connect to %s\n',databaseName)
+  printException (exception)
+  exit(0)
+
+c = conn.cursor()
+sql = """select * from Demo_table"""
+sql_demo = """insert into Demo_table (SR_NO, FIRST_NAME, LAST_NAME, PP_NAME ) values (:0, :1, :2 ,:3)"""
+
+print(Sql_execute)
+print(type(df_excel1))
+c.execute(sql)
+for row in c:
+    print (row[0], "-", row[1])
+print("Print finished, now insertion will start")
+
+c.executemany(sql_demo, df_excel1, batcherrors = True)
+for errorObj in c.getbatcherrors():
+    print("Row", errorObj.offset, "has error", errorObj.message)
+
+print('Insertion has Ended')
+
+c.execute(sql)
+for row in c:
+    print (row[0], "-", row[1])
+
+
+
+
+
+#try:
+  #  print(conn.version.split("."))
+   # c.execute(sql)
+
+#except cx_Oracle.DatabaseError as exception:
+ # printf ('Failed to select from Alti_PayPal_Mapping\n')
+ # printException (exception)
+ # exit(0)
+
+#result=c.fetchall()
+#for row in result:
+#  printf (" %s;%s;%s\n",row[0],row[1],row[2])
+conn.commit()
+conn.close()
+
